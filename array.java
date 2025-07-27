211af7d21b1bf0298f988c13819a64614677a3c4
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2025-07-27 20:02:18 +0530

    hello world program , finding max number by array

---
+++ array.java
@@ -0,0 +1,16 @@
+public class array{
+    public static void main(String[] args) {
+        int arr[]={15,16,89,70};
+        int max=arr[0];
+            for(int i=1;i<4;i++){
+                if(max<arr[i]){
+                    max=arr[i];
+                }else{
+
+                }
+
+            }
+            System.out.println(max);
+    }
+}
+ 
