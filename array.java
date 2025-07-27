35f507a2c4b42c53bc45821d9aa126dc7e722777
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2025-07-27 20:42:19 +0530

    print square,rectangle and triangle patterns

--- array.java
+++ array.java
@@ -13,4 +13,3 @@ public class array{
             System.out.println(max);
     }
 }
- 
---
+++ rectanglepattern.java
@@ -0,0 +1,10 @@
+public class rectanglepattern{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for (int j=1; j<=15;j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
