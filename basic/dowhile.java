64a94f25ed4ca072ebed239ad17d58687d05f133
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2025-08-10 13:03:16 +0530

    loop concept

--- basic/dowhile.java
+++ basic/dowhile.java
@@ -2,7 +2,7 @@ public class dowhile{
     public static void main(String[] args) {
         int i=10;
         do { 
-            System.err.println(i);
+            System.out.println(i);
             i--;
         } while (i>0);
     }
