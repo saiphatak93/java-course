18ae9f076f485fad59ec6463501f7a48b43b761c
Sai Phatak <saiphatak9393@gmail.com>
2025-09-28 09:33:20 +0530

    todays patterns

---
+++ patterns/pattern14.java
@@ -0,0 +1,13 @@
+public class pattern14{
+    public static void main(String[] args) {
+        for (int i = 1; i <=6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = i; k < 5; k++) {
+                System.out.print(k+"   ");
+            }
+            System.out.println("");
+        }
+    }
+}
